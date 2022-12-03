f0a296599d2e4aa6d8f7e36ec1eddafce54772dc
ksenia_subbotina <ks@didlogic.com>
2022-12-03 09:08:31 +0300

    Remove own configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,7 +2,7 @@ import { Config } from './types';
 
 export const exampleConfiguration: Config = {
   notion: {
-    databaseId: '09b2768f6d854a13ba74f0d203c03c10',
-    clientSecret: 'secret_BkqeeIuoLmRhgyNhtcnVvnoqYDWaVMfq4Ic05LKhmTZ',
+    databaseId: '',
+    clientSecret: '',
   },
 };
